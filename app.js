e8bc1c82836e8812965880e5dd1acb2d3278bd3b
elisamoney <elisa_money@hotmail.com>
2020-07-04 16:48:15 -0700

    Add paint feature (step 3)

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const gridContainer_div = document.getElementById("grid-container");
 
+let click = false;
+
 function makeGrid(x) {
-    let userClick = false;
     for (i = 0; i < x**2; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("grid");
+        newDiv.id = i + 1;
+        gridContainer_div.addEventListener("mousedown", () => {click = true});
+        gridContainer_div.addEventListener("mouseup", () => {click = false});
+        newDiv.addEventListener("mouseover", () => paint(newDiv.id));
         // newDiv.innerHTML = i + 1;
         gridContainer_div.appendChild(newDiv);
-        newDiv.addEventListener("mouseover", () => {
-            newDiv.classList.add("hover");
-        });
     }
 }
 
+function paint(a) {
+    if (click === true) {
+        let currGrid = document.getElementById(a);
+        currGrid.classList.add("paint")
+    }
+}
 
 makeGrid(16);
